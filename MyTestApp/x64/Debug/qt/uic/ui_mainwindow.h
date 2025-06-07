3c18c8835c0df5c78dca2fddef3b1601f78f8203
Tahsin <t.erdogdu@live.com>
2025-06-07 23:51:14 +0200

    QT Files

---
+++ MyTestApp/x64/Debug/qt/uic/ui_mainwindow.h
@@ -0,0 +1,95 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.10.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QIcon>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+#include "D3DRenderWidget.h"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionOpen_GTT;
+    QAction *actionAbout;
+    QWidget *centralwidget;
+    D3DRenderWidget *renderWidget;
+    QMenuBar *menubar;
+    QMenu *menuFile;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(506, 600);
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/MainWindow/MyTestApp.ico"), QSize(), QIcon::Mode::Normal, QIcon::State::Off);
+        MainWindow->setWindowIcon(icon);
+        MainWindow->setIconSize(QSize(23, 24));
+        actionOpen_GTT = new QAction(MainWindow);
+        actionOpen_GTT->setObjectName("actionOpen_GTT");
+        actionAbout = new QAction(MainWindow);
+        actionAbout->setObjectName("actionAbout");
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        renderWidget = new D3DRenderWidget(centralwidget);
+        renderWidget->setObjectName("renderWidget");
+        renderWidget->setGeometry(QRect(0, 0, 500, 600));
+        QSizePolicy sizePolicy(QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(renderWidget->sizePolicy().hasHeightForWidth());
+        renderWidget->setSizePolicy(sizePolicy);
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 506, 33));
+        menuFile = new QMenu(menubar);
+        menuFile->setObjectName("menuFile");
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuFile->menuAction());
+        menuFile->addAction(actionOpen_GTT);
+        menuFile->addAction(actionAbout);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "GTT Viewer -Tahsin v0.2", nullptr));
+        actionOpen_GTT->setText(QCoreApplication::translate("MainWindow", "Open GTT", nullptr));
+        actionAbout->setText(QCoreApplication::translate("MainWindow", "About", nullptr));
+        menuFile->setTitle(QCoreApplication::translate("MainWindow", "File", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
